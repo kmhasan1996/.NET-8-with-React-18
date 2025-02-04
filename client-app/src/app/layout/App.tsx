70d8e91f025895e1c31cdda5087c328971cc8468
kmhasan1996 <khandokarmahidyhasan@gmail.com>
2025-02-04 18:02:39 +0600

    Added axios from Section 6.2

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import axios from  'axios'
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import NavBar from './Navbar';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -12,9 +12,14 @@ function App() {
   const[editMode,setEditMode] = useState(false);
   
   useEffect(()=>{
-    axios.get<Activity[]>("http://localhost:5000/api/Activities").then(response=>{
-      console.log(response);
-      setActivities(response.data);
+    agent.Acitivities.list().then(response=>{
+      //console.log(response);
+      let activities:Activity[]=[];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
     })
 
   },[]);
