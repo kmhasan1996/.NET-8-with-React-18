d04043c6377ea534272e88a5b7cb2a4527bd9bd4
kmhasan1996 <khandokarmahidyhasan@gmail.com>
2025-02-12 18:05:02 +0600

    End of 9.3

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -56,8 +56,20 @@ export default observer( function ActivityForm(){
                 <Form.Input type='date' placeholder='Date' value={activity.date}  name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city}  name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue}  name='venue' onChange={handleInputChange}/>
-                <Button loading={loading} floated="right" positive type="submit" content='Submit'/>
-                <Button as={Link} to='/activities' floated="right" type="button" content='Cancel'/>
+                <Button 
+                loading={loading} 
+                floated="right" 
+                positive 
+                type="submit" 
+                content={id ? 'Update': 'Submit'}
+                />
+
+                <Button 
+                as={Link} 
+                to={id ? `/activities/${id}`:'/activities' }
+                floated="right" 
+                type="button" 
+                content='Cancel'/>
             </Form>
         </Segment>
     )
